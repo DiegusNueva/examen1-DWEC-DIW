1738d97d3e5cda9acda8eea39c7af1c3b5ba0dd6
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-14 16:26:43 +0200

    Muy bien. Dos leves mejoras aplicadas.

--- scripts/script.js
+++ scripts/script.js
@@ -7,21 +7,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     //Vinculamos el JS y el template mediante id del botón y del resultado
-    const calculateButton = document.getElementById("calculate");
-    const resultOutput = document.getElementById("result");
+    const calculateButton = document.getElementById("calculate")
+    const resultOutput = document.getElementById("result")
     
     //Vinculamos el evento de al hacer click al botón pase algo:
     calculateButton.addEventListener("click", () => {
     
         //Declaramos las variables que tiene que introducir el usuario
-        let length = (parseFloat(document.getElementById("length").value));
-        let width = (parseFloat(document.getElementById("width").value));
+        let length = (parseFloat(document.getElementById("length").value))
+        let width = (parseFloat(document.getElementById("width").value))
     
         //Si ambos valores son números (ya que se puede introducir e) se harán los cálculos
         if (!isNaN(length) && !isNaN(width)){
-            const area = length * width;
-            const perimeter = (2 *length) + (2*width);
-            const diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2)); //aplicamos las funciones de js para elevar al cuadrado y raiz cuadrada
+            const area = length * width
+            //const perimeter = (2 *length) + (2*width)
+            const perimeter = 2*(length + width)
+            const diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2)) //aplicamos las funciones de js para elevar al cuadrado y raiz cuadrada
 
             //Hacemos que los resultados salgan por pantalla en el div 
             resultOutput.innerHTML = `
@@ -31,8 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             //Si no, mensaje de error
         } else{
-            result.innerText = ` Please, introduce valid values `;
+            result.innerText = ` Please, introduce valid values `
         }
-    });
+    })
     
-    });
+    })
